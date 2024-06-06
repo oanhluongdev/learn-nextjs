ba8632ecdac9df329def1633836083b7daed851d
oanhluongdev <luongtheoanh.dev@gmail.com>
2024-06-06 15:27:32 +0700

    Update header

--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 type NavButtonProps = {
   href: string;
   label: string;
-  isActive: boolean;
+  isActive?: boolean;
 };
 
 export const NavButton = ({ href, label, isActive }: NavButtonProps) => {
-  return <div>{label}</div>;
+  return (
+    <Button
+      asChild
+      size="sm"
+      variant="outline"
+      className={cn(
+        "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition",
+        isActive ? "bg-white/10 text-white" : "bg-transparent"
+      )}
+    >
+      <Link href={href}>{label}</Link>
+    </Button>
+  );
 };
